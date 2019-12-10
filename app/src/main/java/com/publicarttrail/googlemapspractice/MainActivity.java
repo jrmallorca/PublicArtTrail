43183402f378d4ac354b5c191d4cc644052f616a
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2019-12-10 18:53:39 +0000

    Using navbar to change trails

--- app/src/main/java/com/publicarttrail/googlemapspractice/MainActivity.java
+++ app/src/main/java/com/publicarttrail/googlemapspractice/MainActivity.java
@@ -12,57 +12,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-//start page
+// Start page
 public class MainActivity extends AppCompatActivity {
     private Button button;
 
-@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
+
+        // Set up button to go to TrailsActivity
+        button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openTrailsActivity();
             }
         });
-    //ask for permission if required
-    if (ContextCompat.checkSelfPermission(MainActivity.this,
-            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION)){
+
+        // Ask for permission for location, if required
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
             ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }else{
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }
         }
     }
-    }
-    private void openTrailsActivity(){
+
+    private void openTrailsActivity() {
         Intent intent = new Intent(this, TrailsActivity.class);
         startActivity(intent);
     }
 
-
-
+    // Result on whether user accepted permission or not
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults){
-        switch (requestCode){
-            case 1: {
-                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     if (ContextCompat.checkSelfPermission(MainActivity.this,
-                            Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                            Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
                         //Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
                     }
                // }else{
                 //    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
                    // openTrailsActivity();
                 }
+
                 return;
-            }
+
         }
     }
 }
