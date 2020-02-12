de810f85895eca49f832369556a01b8f8e9ddc40
Rithika George <hp18117@bristol.ac.uk>
2020-02-12 23:58:34 +0000

    Removed button from welcome screen

--- app/src/main/java/com/publicarttrail/googlemapspractice/MainActivity.java
+++ app/src/main/java/com/publicarttrail/googlemapspractice/MainActivity.java
@@ -1,34 +1,37 @@
 package com.publicarttrail.googlemapspractice;
-
 import android.content.Intent;
-import android.app.Activity;
 import android.widget.ImageView;
-import android.view.View.OnClickListener;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 //start page
 public class MainActivity extends AppCompatActivity {
-    private Button button;
     private ImageView logo;
+    private Timer timer;
 
-@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(v -> openTrailsActivity());
 
-        logo = findViewById(R.id.logo);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, TrailsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+
+
+        logo = (ImageView) findViewById(R.id.logo);
+
         logo.setImageResource(R.drawable.web_hi_res_512);
     }
 
-    private void openTrailsActivity(){
-        Intent intent = new Intent(this, TrailsActivity.class);
-        startActivity(intent);
-    }
 }
