8a5b54a4e1f98531c19ba2862c8f57fb5149afc2
Jonquil Isys Mallorca <isysrmallorca@gmail.com>
2020-03-09 20:26:45 +0000

    Updated readme

--- app/src/main/java/com/publicarttrail/googlemapspractice/TrailsActivity.java
+++ app/src/main/java/com/publicarttrail/googlemapspractice/TrailsActivity.java
@@ -10,7 +10,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
